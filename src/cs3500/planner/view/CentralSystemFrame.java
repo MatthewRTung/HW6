7c6a35317104bff2dc517f871d9ff668c33fdeb7
MatthewRTung <tung.mat@northeastern.edu>
2024-03-25 20:50:45 -0400

    Clean copy

--- src/cs3500/planner/view/CentralSystemFrame.java
+++ src/cs3500/planner/view/CentralSystemFrame.java
@@ -1,33 +1,33 @@
 package cs3500.planner.view;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import java.awt.Graphics;
+import javax.swing.*;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.BorderLayout;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 import cs3500.planner.model.ReadOnlyCentralSystemModel;
+import cs3500.planner.model.Event;
+import cs3500.planner.xml.XMLConfigurator;
 
 public class CentralSystemFrame extends JFrame implements CentralSystemView {
   private JPanel schedulePanel;
   private JComboBox<String> userDropDown;
   private JButton loadButton;
   private JButton saveButton;
+  private final List<Event> events;
+  private final ReadOnlyCentralSystemModel model;
 
   public CentralSystemFrame(ReadOnlyCentralSystemModel model) {
     super("Planner Central System");
+    this.model = model;
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLayout(new BorderLayout());
+    events = new ArrayList<>();
 
     initializeMenu();
     initializeSchedulePanel();
@@ -41,10 +41,10 @@ public class CentralSystemFrame extends JFrame implements CentralSystemView {
     JMenuBar menuBar = new JMenuBar();
     JMenu fileMenu = new JMenu("File");
 
-    JMenuItem loadMenuItem = new JMenuItem("Load XML");
+    JMenuItem loadMenuItem = new JMenuItem("Add calendar");
     loadMenuItem.addActionListener(this::loadXMLAction);
 
-    JMenuItem saveMenuItem = new JMenuItem("Save Schedules");
+    JMenuItem saveMenuItem = new JMenuItem("Save calendars");
     saveMenuItem.addActionListener(this::saveSchedulesAction);
 
     fileMenu.add(loadMenuItem);
@@ -60,6 +60,7 @@ public class CentralSystemFrame extends JFrame implements CentralSystemView {
       protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawGrid(g);
+        drawEvents(g);
       }
     };
     schedulePanel.setPreferredSize(new Dimension(800, 600));
@@ -76,20 +77,67 @@ public class CentralSystemFrame extends JFrame implements CentralSystemView {
       for (int j = 0; j < cols; j++) {
         g.drawRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
       }
+      if (i % 4 == 0) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, i * cellHeight - 1, schedulePanel.getWidth(), 2);
+      }
+    }
+  }
+
+  private void drawEvents(Graphics g) {
+    int cellWidth = schedulePanel.getWidth() / 7;
+    int cellHeight = schedulePanel.getHeight() / 24;
+    g.setColor(Color.RED);
+    for (Event event : events) {
+      LocalDateTime startTime = event.getStartTime();
+      LocalDateTime endTime = event.getEndTime();
+      int dayCol = startTime.getDayOfWeek().getValue() % 7;
+      int startHour = startTime.getHour();
+      int endHour = endTime.getHour();
+      int daySpan = endTime .getDayOfWeek().getValue() - startTime.getDayOfWeek().getValue();
+      if (daySpan < 0) {
+        daySpan += 7;
+      }
+      for (int day = 0; day <= daySpan; day++) {
+        int column = (dayCol + day) % 7;
+        int startY = startHour * cellHeight;
+        int endY;
+        if (day < daySpan) {
+          endY = 24 * cellHeight;
+        } else {
+          endY = endHour * cellHeight;
+        }
+        int rectHeight = endY - startY;
+        g.fillRect(column * cellWidth, startY, cellWidth, rectHeight);
+      }
     }
   }
 
   private void initializeControlPanel() {
-    JPanel controlPanel = new JPanel();
-    loadButton = new JButton("Load Schedule");
-    saveButton = new JButton("Save Schedule");
-
+    JPanel controlPanel = new JPanel(new GridLayout(1, 0, 5, 0));
+    loadButton = new JButton("Create event");
+    saveButton = new JButton("Schedule event");
+    //takes list of user names to pick from
+    userDropDown = new JComboBox<String>(); // when no user is selected, should say <none>
+    userDropDown.addItem("<none>"); // new
+    for (String userName : model.getUserName()) {
+      userDropDown.addItem(userName);
+    }
+    userDropDown.addActionListener(e -> {
+      String selectedUser = Objects.requireNonNull(userDropDown.getSelectedItem()).toString();
+      if (!"<non>".equals(selectedUser)) {
+        loadUserSchedule(selectedUser);
+      }
+    });
     loadButton.addActionListener(this::loadXMLAction);
     saveButton.addActionListener(this::saveSchedulesAction);
 
+    controlPanel.add(userDropDown); // new
     controlPanel.add(loadButton);
     controlPanel.add(saveButton);
 
+    loadButton.setHorizontalAlignment(SwingConstants.CENTER); // new
+    saveButton.setHorizontalAlignment(SwingConstants.CENTER); // new
     this.add(controlPanel, BorderLayout.SOUTH);
   }
 
@@ -98,8 +146,16 @@ public class CentralSystemFrame extends JFrame implements CentralSystemView {
     int option = fileChooser.showOpenDialog(this);
     if (option == JFileChooser.APPROVE_OPTION) {
       File selectedFile = fileChooser.getSelectedFile();
+      XMLConfigurator configurator = new XMLConfigurator();
+      List<Event> events = configurator.readXMLFile(selectedFile.getAbsolutePath());
       System.out.println("Selected file: " + selectedFile.getAbsolutePath());
     }
+  }
+
+  private void loadUserSchedule(String userId) {
+    events.clear();
+    events.addAll(model.getEventsForUser(userId));
+    repaint();
   }
 
   private void saveSchedulesAction(ActionEvent e) {
